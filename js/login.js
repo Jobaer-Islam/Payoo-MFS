0afe45f400e63bba0b05d1fcf13ff4996f75ecca
Jobaer Islam <jobaerislam16@gmail.com>
2025-03-05 21:36:31 +0600

    log in form done

---
+++ js/login.js
@@ -0,0 +1,10 @@
+// step 1: Id কে ধরছি getElementById দিয়ে
+document.getElementById("btn-login").addEventListener("click",
+    function (event) {
+        event.preventDefault();
+        const accountNUmber = document.getElementById("Account-Number").value;
+        const pin = document.getElementById("pin").value;
+
+
+
+    });
