e5b092349074018c0a967bfd8756de62046fe796
Jobaer Islam <jobaerislam16@gmail.com>
2025-03-05 22:40:08 +0600

    done navbar

--- js/login.js
+++ js/login.js
@@ -4,7 +4,15 @@ document.getElementById("btn-login").addEventListener("click",
         event.preventDefault();
         const accountNUmber = document.getElementById("Account-Number").value;
         const pin = document.getElementById("pin").value;
-
-
+        const convertedPin = parseInt(pin);
+        if (accountNUmber.length === 11) {
+            if (convertedPin === 1234) {
+                window.location.href = "main.html";
+            } else {
+                alert("pin thik nai")
+            }
+        } else {
+            alert("need valid account number")
+        }
 
     });
